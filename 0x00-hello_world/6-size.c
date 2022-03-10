41643703154e0f6fbfb7fbda0607c126286b8e75
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-10 13:19:26 -0800

    Print size of data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int lt;
+	long long int llt;
+	float f;
+
+	printf("Size of a char: %lu\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu\n", (unsigned long)sizeof(lt));
+	printf("Size of a long long int: %lu\n", (unsigned long)sizeof(llt));
+	printf("Size of a float: %lu\n", (unsigned long)sizeof(f));
+	return  (0);

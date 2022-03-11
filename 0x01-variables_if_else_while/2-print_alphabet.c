a53f6dc360ba76e4d099b3f586e885be43ff2fd2
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-11 04:49:14 -0800

    Print all letters of the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0 Always(Success)
+ */
+int main(void)
+{
+	char c;
+	for (c == "A", c <="Z", ++c)
+	{
+		printf("%c", c);
+	}
+	return (0);
+}

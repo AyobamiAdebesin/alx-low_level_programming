5d4ffc9b720fa6fe31f87b2f51df2c2a9942eeaa
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-11 04:06:17 -0800

    Check for parity

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX /2;
+	if (n > 0)
+	{
+		printf("%d, is positive\n", n);
+	}
+	else if(n == 0)
+	{
+		printf("%d, is 0", n);
+	}
+	else
+	{
+		printf("%d, is negative", n);
+	}
+
+	return (0);
+}

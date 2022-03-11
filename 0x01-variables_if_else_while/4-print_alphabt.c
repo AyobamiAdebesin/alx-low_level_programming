1c1169d1a1c70665552cd6e78ebf378060db58c6
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-11 06:12:56 -0800

    Print alphabets exept e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,15 +10,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 97; i <= 122; ++i)
+	for (i = 97; i <= 123; ++i)
 	{
-		if (i == 102 && i == 113)
+		if (i != 101 && i != 113)
 		{
-			continue;
-		}
-		else{
 			putchar(i);
 		}
 	}
+	putchar("\n");
 	return (0);
 }

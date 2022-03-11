22857dee9598317064cdbdb88a43d32835ef32ce
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-11 07:48:02 -0800

    Print the numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 30; i < 40; i++)
+	{
+		putchar(i);
+	}
+	for (j = 97; j < 103; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n')
+	return (0);
+}

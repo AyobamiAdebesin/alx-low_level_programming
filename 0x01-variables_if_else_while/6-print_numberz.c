4f106a8f23bf85f6eefafdd74dc3918bf2b41899
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-11 06:34:51 -0800

    Print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	pucchar('\n');
+	return (0);
+}

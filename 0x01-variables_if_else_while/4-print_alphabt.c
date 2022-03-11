d7a52d7c2d5fa25e6c568c8c3a63c9ff608f6c0b
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-11 06:03:18 -0800

    Print alphabets exept e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0 Always(Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; ++i)
+	{
+		if (i == 102 && i == 113)
+		{
+			return;
+		}
+		else{
+			putchar(i);
+		}
+	}
+	return (0);
+}

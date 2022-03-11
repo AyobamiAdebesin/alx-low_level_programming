09799bd98043ad5878aec44da77e2b47a2e9bec7
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-11 07:30:49 -0800

    Print alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0 Always(Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i <= 97; --i)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

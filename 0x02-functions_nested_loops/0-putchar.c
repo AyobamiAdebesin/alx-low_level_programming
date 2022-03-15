a0a8073072f60321d66469b2d42ac309d9fd5fcb
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-15 05:49:34 -0700

    Print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,15 +2,16 @@
 /**
  * main - Entry Point
  *
- * Return Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int chr_val[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int count, sz;
+
 	sz = sizeof(chr_val) / sizeof(int);
 
-	for (count=0; count < sz; count++)
+	for (count = 0; count < sz; count++)
 	{
 		_putchar(chr_val[count]);
 	}

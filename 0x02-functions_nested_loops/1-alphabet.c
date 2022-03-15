70630135c20613fd394bb984080ff868163845c6
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-15 06:13:38 -0700

    Print the alphabets in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int count = 97;
+
+	while (count < 113)
+	{
+		_putchar(count);
+		count ++;
+	}
+	_putchar('\n');
+
+	return (0);
+}

6e7a78f958e1f6ee93dbb38e74a5de0e4312bff8
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-15 06:45:43 -0700

    Print the alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_main_alphabet(void)
 	int count = 97;
 	int loop_print;
 	
-	for (loopcount = 0; loop_count <= 10; loop_count++)
+	for (loop_count = 0; loop_count <= 10; loop_count++)
 	{
 		while (count < 123)
 		{

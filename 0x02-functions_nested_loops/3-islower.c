0e4af9a1442da0a98d17dcf7ac33fbd57d7e42a1
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-15 21:34:09 +0100

    Check for lower case and uppercase of an alphabet

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - Check if an alphabet is lower case or upper case
+ *
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

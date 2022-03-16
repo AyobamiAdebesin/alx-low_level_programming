76f06d2963ddd55a4c719925cbb6a9bbbeda4df1
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-16 04:31:41 -0700

    Print all the minutes of one day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * jack_bauer - Print every minute of the day of Jack Bauer
+ *
+ * Return: Every minute of the day of Jack Bauer (0<=a<=2, 0<=b<=9, 0<=c<=5, 0<=d<= 9)
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+	
+	for (a = 48; a <= 50, a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			for (c = 48; c <= 53; c++)
+			{
+				for (d = 48,; d <= 57; d++)
+				{
+					if (a == 50 && b == 51 && c == 53 && d == 57)
+					{
+						break;
+					}
+					else
+					{
+						_putchar(a);
+						_putchar(b);
+						_putchar(c);
+						_putchar(d);
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
+}

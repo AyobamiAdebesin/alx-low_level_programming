35fb1c1b3f42e6bfed0184b2b61d71e370540171
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-16 04:32:55 -0700

    Print all the minutes of one day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,13 +8,13 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 	
-	for (a = 48; a <= 50, a++)
+	for (a = 48; a <= 50; a++)
 	{
 		for (b = 48; b <= 57; b++)
 		{
 			for (c = 48; c <= 53; c++)
 			{
-				for (d = 48,; d <= 57; d++)
+				for (d = 48; d <= 57; d++)
 				{
 					if (a == 50 && b == 51 && c == 53 && d == 57)
 					{

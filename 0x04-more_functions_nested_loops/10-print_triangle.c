cdae10f34952c52010075c7d2dd6b065fa497123
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-18 16:41:24 -0700

    Print triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_diagonal - Print a diagonal line on the terminal
+ * @n: The number of lines to be printed
+ * Return: The diagonal line
+ */
+void print_triangle(int size)
+{
+	int count;
+	int count_space;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (count = 0; count < n; count++)
+		{
+			for (count_space = 0; count_space < n; count_space++)
+			{
+				if (count_space == count)
+				{
+					_putchar(' ');
+				}
+				else if (count_space < count)
+				{
+					_putchar(35);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}

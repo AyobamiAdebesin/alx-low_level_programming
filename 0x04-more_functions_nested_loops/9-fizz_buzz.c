5f67af9325a89d7cb98708b18aab223456074070
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-18 16:11:25 -0700

    Print FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,10 +7,10 @@
 int main(void)
 {
 	int count;
+
 	count = 1;
 	printf("%d", count);
-
-	for (count = 2; count <= 100; count++)
+	ifor (count = 2; count <= 100; count++)
 	{
 		if (count % 5 == 0)
 		{
@@ -29,7 +29,7 @@ int main(void)
 		}
 		else
 		{
-			printf("%d ", count);
+			printf(" %d ", count);
 		}
 	}
 	printf("\n");

e12a8eead1a3955c1a17e747fbb5c80e5153b645
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-18 07:20:57 -0700

    Print diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,7 +10,7 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 	{
-		putchar(32);
+		_putchar(32);
 	}
 	else
 	{
@@ -19,11 +19,11 @@ void print_diagonal(int n)
 			count_space = count;
 			while (count_space > 0)
 			{
-				putchar(32);
+				_putchar(32);
 				count_space--;
 			}
-			putchar(92);
-			putchar('\n');
+			_putchar(92);
+			_putchar('\n');
 		}
 	}
 }

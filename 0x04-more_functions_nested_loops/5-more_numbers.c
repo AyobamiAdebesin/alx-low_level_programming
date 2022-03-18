45d53a932a3c38a1d64f56fb61939ac5572b3a4c
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-18 05:10:28 -0700

    Print more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,14 @@ void more_numbers(void)
 	{
 		for (ch = 48; ch < 62; ch++)
 		{
+			if (ch > 57)
+			{
+				_putchar(49);
+				_putchar(ch - 10);
+			}
+			
 			_putchar(ch);
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }

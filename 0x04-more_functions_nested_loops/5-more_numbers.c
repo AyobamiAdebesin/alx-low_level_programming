646c620325dbf6c1ccc140e7706dbc304acae5ac
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-18 05:54:28 -0700

    Print more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,6 +3,7 @@
  * more_numbers - Print more numbers
  * Return: 0 Always Success
  */
+
 void more_numbers(void)
 {
 	int count;
@@ -10,13 +11,13 @@ void more_numbers(void)
 
 	for (count = 1; count < 11; count++)
 	{
-		for (ch = 48; ch < 62; ch++)
+		for (ch = 0; ch < 15; ch++)
 		{
-			if (ch > 57)
+			if (ch > 10)
 			{
 				_putchar(49);
 			}
-			_putchar(ch - 10);
+			_putchar((ch % 10) + 48);
 		}
 		_putchar('\n');
 	}

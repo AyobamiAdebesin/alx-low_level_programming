cd46f09dc59ad8cdb2e22362c4064f0b4bc16ec7
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-18 16:53:24 -0700

    Print triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,20 +4,21 @@
  * @n: The number of lines to be printed
  * Return: The diagonal line
  */
+
 void print_triangle(int size)
 {
 	int count;
 	int count_space;
 
-	if (n <= 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (count = 0; count < n; count++)
+		for (count = 0; count < size; count++)
 		{
-			for (count_space = 0; count_space < n; count_space++)
+			for (count_space = 0; count_space < size; count_space++)
 			{
 				if (count_space == count)
 				{

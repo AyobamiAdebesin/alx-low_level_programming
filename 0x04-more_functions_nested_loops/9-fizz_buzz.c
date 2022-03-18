f9945538d4b03627a55eb95ff37edf935b1e9661
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-18 15:45:17 -0700

    Print FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * FizzBuzz - Execute the fizz buzz code
+ * Return: The Fizz Buzz output
+ */
+void FizzBuzz(void)
+{
+	int count;
+
+	for (count = 1; count <= 100; count++)
+	{
+		if (count % 5 == 0)
+		{
+			if (count % 3 == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			else
+			{
+				printf("Fizz ");
+			}
+		}
+		else if (count %5 != 0)
+		{
+			printf("%d ", count);
+		}
+	}
+}

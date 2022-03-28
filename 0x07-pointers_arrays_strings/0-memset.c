1e44e30ae7cca8493dae5656ebf1dbccea4f0b5b
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-28 07:15:48 -0700

    memset from scratch

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,13 @@
 #include "main.h"
-/*
- * _memset() - A function that fills the first n bytes
+/**
+ * _memset - A function that fills the first n bytes
  * @s: The address of the memory to be filled
  * @n: The first n bytes of memory
- * Return - A pointer to the memory area
+ * @b: The chracter to fill with
+ *
+ * Return: A pointer to the memory area
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int count;

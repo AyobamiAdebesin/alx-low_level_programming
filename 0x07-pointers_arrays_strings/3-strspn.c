e8816161007869eb532edd833d368d7da438eb52
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-29 14:25:22 -0700

    strspn from scratch

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,7 +9,7 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j, bool;
 
-	for (i = 0l *(s + i) != '\0'; i++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		bool = 1;
 		for (j = 0; *(accept + j) != '\0'; j++)

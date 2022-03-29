fa30d0e798f693e118e258a1c53ed130c0228fbd
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-29 22:43:56 +0100

    strbrk from scratch

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,3 +4,4 @@ char* _strchr(char *, char);
 unsigned int _strspn(char *s, char *accept);
 char* _strstr(char *, char *);
 void print_diagsums(int *a, int size);
+char* _strpbrk(char *s, char *accept)

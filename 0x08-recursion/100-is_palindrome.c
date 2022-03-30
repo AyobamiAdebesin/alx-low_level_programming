9e3e61dc0dfde2956e0f0524857bf28fab66c861
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-30 16:46:50 -0700

    Check for recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,54 @@
+#include "main.h"
+/**
+ * _strlen - Returns the length of a string recursively
+ * @s: The string
+ * Return: the length of the string s 
+ */
+
+int _str_len(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _str_len(s + 1));
+	}
+	else
+		return (0);
+}
+
+/** 
+ * comparator - Compares a stringusing the first and last character
+ * @s: The string pointer
+ * @n1: The starting index
+ * @n2: The last index
+ * Return: 1 or 0
+ */
+
+int comparator(char* s, int n1, int n2)
+{
+	if (*(s + n1) == *(s + n2))
+	{
+		if (n1 == n2 || n2 = n1 + 1)
+		{
+			return (1);
+		}
+		return (0 + comparator(s, n1 + 1, n2 - 1));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - Check if a string is palindrome using the above functions
+ * @s: The string to be checked
+ * Return: 1 or 0
+ */
+
+int is_palindrome(char *s)
+{
+	len_s = _str_len(s);
+	
+	if (*s == '\0')
+	{
+		return (1);
+	}
+	return (comparator(s, 0, len_s - 1));
+}

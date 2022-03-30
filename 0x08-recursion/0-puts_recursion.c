4c3cee7de9803e38590c268a388eb1ef14236691
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-30 04:16:23 -0700

    Print a string recursively

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,13 @@
 void _puts_recursion(char *s)
 {
 	_putchar(*s);
-	
-	if (*(s++) == '\0')
+
+	if (*(s + 1) == '\0')
 	{
+		_putchar('\n');
 		return;
 	}
-
-       	_puts_recursion(s++);
+	_puts_recursion(s++);
 
 }
 

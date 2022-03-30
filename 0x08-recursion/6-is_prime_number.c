1c171a72ae8e09eecb39ac434a7efa33fdc0b047
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-30 07:15:57 -0700

    Check for prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,10 +7,13 @@
 
 int is_prime_number(int n)
 {
-	if (n == 1 || n * -1 == n) 
+	if (n == 1) 
 		return (0);
-	else if (n % 1 == 0 && n % n == 0)
-		return (1);
+	if (n > 0)
+	{	
+		if (n % 1 == 0 && n % n == 0)
+			return (1);
+	}
 	else
 		return (0);
 }

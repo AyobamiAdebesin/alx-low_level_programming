81005c766b3ac127361e99a061f29b09b114cb4e
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-30 17:46:57 -0700

    Print the square root of a natural number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,40 @@
 #include "main.h"
 /**
- * _pow_recursion - Print the power of an integer recursively
- *@x: Base integer
- *@y: power integer
- *Return: The power of x raise to y
+ * power_operation - Returns the square root of a natural number
+ * @n: the integer
+ * @c: The power iterator
+ * Return: the square root
  */
 
-int 
+int power_operation(int n, int c)
+{
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+		{
+			return (c);
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+	return (0 + power_operation(n, c + 1));
+}
+
+/**
+ * _sqrt_recursion - Returns the square root of an integer
+ * @n: The integer
+ * Return: the square root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 1)
+		return (1);
+	if (n == 0)
+		return (0);
+	if (n < 0)
+		return (-1);
+	return (power_operation(n, 2));
+}

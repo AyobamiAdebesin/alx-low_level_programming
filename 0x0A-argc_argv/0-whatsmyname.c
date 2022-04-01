ff8617446c8fbb854e6752881d35ecdb2d4df327
AyobamiAdebesin <froshofficial@gmail.com>
2022-04-01 00:45:01 -0700

    Print the name of the file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - The main function
+ * @argc: The argument count
+ * @argv: The  argument vector
+ * Return: 0(Success)
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

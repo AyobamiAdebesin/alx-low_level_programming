e9e1e4bbef60e3b56013f57c0c26429b67948979
AyobamiAdebesin <froshofficial@gmail.com>
2022-04-01 02:51:43 -0700

    add numbers from the command line as arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - The main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 or 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+	int sum;
+	int val;
+
+	if (argc < 2)
+		printf("%d\n", 0);
+	else
+	{
+		for (count = 1; count < argc; count++)
+		{
+			if (*(argv[count]) >= 'a' && *(argv[count]) <= 'z')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+				sum += atoi(argv[count]);
+		}
+		printf("%i\n", sum);
+	}
+	return (0);
+}
+

f04ae83d94bc1efd2c8a2218b7354ab7d29c5c5c
AyobamiAdebesin <froshofficial@gmail.com>
2022-04-01 14:25:32 +0100

    Add numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,33 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <ctype.h>
 /**
- * main - The main function
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 or 1
+ * main - adds positive numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-
 int main(int argc, char *argv[])
 {
-	int count;
-	int sum;
+	int i, j, add = 0;
 
-	if (argc < 2)
-		printf("%d\n", 0);
-	else
+	for (i = 1; i < argc; i++)
 	{
-		for (count = 1; count < argc; count++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (*(argv[count]) >= 'a' && *(argv[count]) <= 'z')
+			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
 				return (1);
 			}
-			else
-				sum += atoi(argv[count]);
 		}
-		printf("%i\n", sum);
+		add += atoi(argv[i]);
 	}
+	printf("%d\n", add);
 	return (0);
 }

a0b12e3d068a453e141e5679fd6dbfd278004519
AyobamiAdebesin <froshofficial@gmail.com>
2022-03-31 21:56:43 -0700

    _puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,15 +8,14 @@
 void _puts_recursion(char *s)
 {
 
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return 0;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 	else
 	{
-		_putchar(*s++);
-		_puts_recursion(s++);
+		_putchar('\n');
 	}
 
 }

62234cc532eaabd093a63ba19901cd4180a4e7ba
AyobamiAdebesin <froshofficial@gmail.com>
2022-04-07 05:56:45 -0700

    Concat two strings together

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
@@ -20,7 +21,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	
 	for (i = 0; *(s1 + i) != '\0', i++)
 		;
 	for (j = 0; *(s2 + j) != '\0', j++)

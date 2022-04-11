20ba381e53a4f0642d8672dd05af6d6f449ef173
AyobamiAdebesin <froshofficial@gmail.com>
2022-04-11 15:02:31 +0100

    Absolute value with macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
-// #ifndef abs_macro
-// #define abs_macro
-// #define ABS(x) (if ((x) > 0) \
-//                 return (x); \
-//                 if (x < 0) \
-//                 return (-1 * x)
-// #endif
+#ifndef abs_macro
+#define abs_macro
+#define ABS(x) (if ((x) > 0) \
+                return (x); \
+                if (x < 0) \
+                return (-1 * x)
+#endif

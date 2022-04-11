b71a7821f2fe44e0e2fe533cc2f2a70c27aba83e
AyobamiAdebesin <froshofficial@gmail.com>
2022-04-11 08:42:28 -0700

    Struct for a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - struct of a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: A struct for a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif

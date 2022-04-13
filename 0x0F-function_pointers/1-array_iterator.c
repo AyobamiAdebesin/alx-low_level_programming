c8a34b54977e5b96cf0a0b1b89c193e36f8b8703
AyobamiAdebesin <froshofficial@gmail.com>
2022-04-14 00:21:10 +0100

    Iterate over an array with function pointer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,13 +1,12 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a
- * parameter on each element of an array.
+ * array_iterator - Iterates over an array with a function pointer.
  * @array: array to be iterated.
  * @size: size of the array.
  * @action: pointer to the function.
  *
- * Return: no return.
+ * Return: void.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {

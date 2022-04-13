c7851b34b4ab5986a4cf7155784ee524209d8101
AyobamiAdebesin <froshofficial@gmail.com>
2022-04-14 00:17:17 +0100

    Iterate over an array with function pointer

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,8 @@
 #ifndef _FUNCTION_POINTER_
 #define _FUNCTION_POINTER_
+
+#include <stdlib.h>
+#include <stddef.h>
 int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));

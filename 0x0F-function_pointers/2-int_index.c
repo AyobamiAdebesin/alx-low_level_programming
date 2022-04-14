c78b1fcb21640d91855b41f5a600377f2e1654a8
AyobamiAdebesin <froshofficial@gmail.com>
2022-04-13 17:27:56 -0700

    Function that searches for a string

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "funtion_pointers.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * int_index - Searches for an integer
+ * @array: The array we need to search
+ * @size: The size of the array
+ * @cmp: The function pointer to be used
+ * Return: 0, -1, and int 
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+
+	if (array && cmp)
+	{
+		if (size <= 0)
+			return (-1);
+		for (index = 0; index < size; index++)
+		{
+			if (cmp(array[index]) != 0)
+				return (index);
+			else
+				retrn (-1);
+		}
+	}
+}

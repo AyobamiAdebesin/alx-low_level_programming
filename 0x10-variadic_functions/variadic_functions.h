3bb351c15841b699ba916bfdc5b5b1f600fee336
AyobamiAdebesin <froshofficial@gmail.com>
2022-04-15 03:11:12 +0100

    A function that print strings

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,4 +4,5 @@
 #include <stdio.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 #endif

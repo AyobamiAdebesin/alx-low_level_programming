aa6a31fbbe39d75253682c9a1dfadbc89ad19dc9
AyobamiAdebesin <froshofficial@gmail.com>
2022-04-21 04:09:20 -0700

    a function that prints the elements of asingly linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - Print the elements of a singly linked list
+ * @h: Pointer to a node
+ * Return: The number of nodes in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("[%d] %s", h->len, h->str);
+	count++;
+	}
+	return (count);
+}

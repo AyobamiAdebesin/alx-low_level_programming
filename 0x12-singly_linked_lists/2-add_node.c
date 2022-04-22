550fc0f2225c3be34a161b5bca08d19b24310922
AyobamiAdebesin <froshofficial@gmail.com>
2022-04-22 06:07:32 +0100

    Add node at the begninning of a list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * add_node - adds a new node at the beginning
- * of a list_t list.
+ * of a list_t list
  * @head: head of the linked list.
  * @str: string to store in the list.
  * Return: address of the head.

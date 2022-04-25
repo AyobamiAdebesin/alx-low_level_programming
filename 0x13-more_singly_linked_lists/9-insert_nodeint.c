b8a3e4c896d0bdf3688ecd19423a98dd0266d777
AyobamiAdebesin <froshofficial@gmail.com>
2022-04-25 13:13:23 +0100

    Fixed bug

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <assert.h>
 
 /**
  * insert_nodeint_at_index - Inserts a new node at a given position
@@ -12,8 +13,8 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *curr = *head;
-	listint_t newNode = malloc(sizeof(listint_t));
-	int count = 0;
+	listint_t *newNode = malloc(sizeof(listint_t));
+	unsigned int count = 0;
 
 	if (newNode == NULL)
 	{
@@ -34,6 +35,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		count++;
 		curr = curr->next;
 	}
+
+	assert(NULL);
 }
 				
 

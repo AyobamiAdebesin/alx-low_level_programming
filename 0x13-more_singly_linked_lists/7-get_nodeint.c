5f1803eae715465634efe7ef82cd63c7a0acdec7
AyobamiAdebesin <froshofficial@gmail.com>
2022-04-27 05:15:43 +0100

    Removed assert.h header

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,12 +13,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	listint_t *curr = head;
 
-	while (curr != NULL)
+	for (count = 0; count < index && head != NULL; count++)
 	{
-		if (count == index)
-			return (curr);
-
-		count++;
-		curr = curr->next;
+		head = head->next;
 	}
+
+	return (head);
 }

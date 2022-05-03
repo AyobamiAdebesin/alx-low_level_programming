989e7950b66be45b3d70cfc7056bfe06eaaac23c
AyobamiAdebesin <froshofficial@gmail.com>
2022-05-03 05:46:35 +0100

    added new project files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,17 +1,38 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * create_file - A function that creates a file
- * @filename: The name of the file to be created
- * @text_content: The content of the file 
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
  *
- * Return: 1 on success or -1 on failure
+ * Return: 1 if it success. -1 if it fails.
  */
-
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
+	int nletters;
+	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	fd = open(filename 
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}

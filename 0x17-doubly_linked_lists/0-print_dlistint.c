32c398e8e3c83bee98417068a41846154fd81484
AyobamiAdebesin <froshofficial@gmail.com>
2022-06-10 05:34:17 +0100

    print the nodes in a doubly linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements of a doubly linked list
+ * print_dlistint - prints all the elements
+ * of a doubly linked list
  *
  * @h: head of the list
  * Return: the number of nodes in the list
@@ -24,6 +25,6 @@ size_t print_dlistint(const dlistint_t *h)
 		count++;
 		h = h->next;
 	}
-	
+
 	return (count);
 }

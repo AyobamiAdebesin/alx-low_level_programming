ba4ab27ce6687f0b04693d9878e1ea72ce50d931
AyobamiAdebesin <froshofficial@gmail.com>
2022-10-05 14:39:17 +0100

    Removed main function

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -20,22 +20,14 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 	}
 
-	for(i=0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		if (*(array+i) == value)
+		if (*(array + i) == value)
 		{
 			printf("Element found at position: %lu", i);
 			return (i);
 		}
 	}
-	
-	return (-1);
-}
 
-int main(void)
-{
-	int array[] = {10, 1, 42, 3, 4, 42, 6, 7, -1, 9};
-	size_t size = sizeof(array) / sizeof(array[0]);
-	printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
-	return (EXIT_SUCCESS);
+	return (-1);
 }

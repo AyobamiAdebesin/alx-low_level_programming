36f8d44b3a990d77f62b366e097433f0a81eb1ef
AyobamiAdebesin <froshofficial@gmail.com>
2022-10-05 15:50:01 +0100

    Removed printf

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -25,7 +25,6 @@ int linear_search(int *array, size_t size, int value)
 		printf("Value checked array[%lu] = [%d]\n", i, *(array + i));
 		if (*(array + i) == value)
 		{
-			printf("Found %d at index: %lu\n", value, i);
 			return (i);
 		}
 	}
